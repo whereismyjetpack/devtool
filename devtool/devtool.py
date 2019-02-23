0f6a5ed956eca18dac2e048809fe5db7bf54db18
Dann Bohn <dannbohn@gmail.com>
2019-02-23 18:36:44 -0500

    refactor to use click

--- devtool/devtool.py
+++ devtool/devtool.py
@@ -1,16 +1,3 @@
-"""
-Usage:
-  devtool  [options]
-  devtool show-config
-  devtool fhr
-  devtool init
-
-Options:
-  --skip-compile                      Skip Compile
-  --skip-docker                       Skip Docker
-  --skip-helm                         Skip Helm
-  --suppress-output
-"""
 from __future__ import absolute_import
 import collections
 from config import build_config
@@ -23,39 +10,40 @@ import yaml
 import uuid
 from termcolor import colored, cprint
 import subprocess
-from docopt import docopt
 from helm import build_helm_command, helm_setup
 from minikube import minikube_setup
-from command import run
-from command import check_output
+from command import run, check_output
 from fluxhelmrelease import build_fhr
+import click
+from click_default_group import DefaultGroup
 
+cfg = build_config()
 
-def parse_args():
-    arguments = docopt(__doc__, version="0.0.1")
-    return arguments
-
+@click.group(cls=DefaultGroup, default='build', default_if_no_args=True)
 def main():
-    arguments = parse_args()
-    cfg = build_config(arguments)
+    pass
+
+@main.command()
+def init():
+    print("init")
+
+@main.command()
+def fhr():
+    build_fhr(cfg)
+
+@main.command()
+def show_config():
+    print(yaml.dump(cfg, default_flow_style=False))
+
+@main.command()
+@click.option("--skip-compile", default=False, is_flag=True, help="Skip compiling the software")
+@click.option("--skip-docker", default=False, is_flag=True, help="Skip Docker build phase")
+@click.option("--skip-helm", default=False, is_flag=True, help="Skip helm install phase")
+def build(skip_compile, skip_docker, skip_helm):
+
     minikube = minikube_setup(cfg)
 
-    helm_setup(cfg)
-
-    if arguments["show-config"]:
-        print(yaml.dump(cfg, default_flow_style=False))
-        sys.exit(0)
-
-    if arguments["fhr"]:
-        print(build_fhr(cfg))
-        sys.exit(0)
-
-    if arguments["init"]:
-        initialize(cfg)
-        sys.exit(0)
-
-
-    if cfg["skip"]["compile"]:
+    if skip_compile:
         print(colored("Skipping Compile Phase", "red"))
     else:
         compile_command = cfg["compile"]["command"]
@@ -68,7 +56,7 @@ def main():
         )
         run(compile_command.split(), cfg)
 
-    if cfg["skip"]["docker"]:
+    if skip_docker:
         print(colored("Skipping Docker Phase", "red"))
     else:
         # TODO set defaults on docker vars if minikube doesn't provide them. .get(foo, bar)
@@ -76,12 +64,13 @@ def main():
         docker_command = f"docker --host={minikube['DOCKER_HOST']} --tlsverify={minikube['DOCKER_TLS_VERIFY']} --tlscacert={minikube['DOCKER_CERT_PATH']}/ca.pem --tlscert={minikube['DOCKER_CERT_PATH']}/cert.pem --tlskey={minikube['DOCKER_CERT_PATH']}/key.pem build -t {cfg['docker']['image']}:{cfg['docker']['tag']} ."
         run(docker_command.split(), cfg)
 
-    if cfg["skip"]["helm"]:
+    if skip_helm:
         print(colored("Skipping Helm Phase", "red"))
     else:
         print(colored(f"Installing helm chart", "yellow", attrs=["bold"]))
         helm_command = build_helm_command(cfg)
         run(helm_command, cfg)
+
 
 
 if __name__ == "__main__":
