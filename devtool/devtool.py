b3dc71b705661e15b18b476639f3b8e78a562192
Dann Bohn <dannbohn@gmail.com>
2019-02-23 19:06:43 -0500

    remove termcolor dependency

--- devtool/devtool.py
+++ devtool/devtool.py
@@ -8,7 +8,6 @@ import sys
 import json
 import yaml
 import uuid
-from termcolor import colored, cprint
 import subprocess
 from helm import build_helm_command, helm_setup
 from minikube import minikube_setup
@@ -44,30 +43,23 @@ def build(skip_compile, skip_docker, skip_helm):
     minikube = minikube_setup(cfg)
 
     if skip_compile:
-        print(colored("Skipping Compile Phase", "red"))
+        click.echo(click.style("Skipping Compile Phase", fg="red"))
     else:
         compile_command = cfg["compile"]["command"]
-        print(
-            colored(
-                f"Compiling software with the following command: {compile_command}",
-                "yellow",
-                attrs=["bold"],
-            )
-        )
+        click.echo(click.style(f"Compiling software with the following command: {compile_command}", fg="yellow"))
         run(compile_command.split(), cfg)
 
     if skip_docker:
-        print(colored("Skipping Docker Phase", "red"))
+        click.echo(click.style("Skipping Docker Phase", fg="red"))
     else:
-        # TODO set defaults on docker vars if minikube doesn't provide them. .get(foo, bar)
-        print(colored(f"Building the docker container", "yellow", attrs=["bold"]))
+        click.echo(click.style("Building the Docker Container", fg="yellow"))
         docker_command = f"docker --host={minikube['DOCKER_HOST']} --tlsverify={minikube['DOCKER_TLS_VERIFY']} --tlscacert={minikube['DOCKER_CERT_PATH']}/ca.pem --tlscert={minikube['DOCKER_CERT_PATH']}/cert.pem --tlskey={minikube['DOCKER_CERT_PATH']}/key.pem build -t {cfg['docker']['image']}:{cfg['docker']['tag']} ."
         run(docker_command.split(), cfg)
 
     if skip_helm:
-        print(colored("Skipping Helm Phase", "red"))
+        click.echo(click.style("Skipping Helm Phase", fg="red"))
     else:
-        print(colored(f"Installing helm chart", "yellow", attrs=["bold"]))
+        click.echo(click.style("Installing Helm Chart", fg="yellow"))
         helm_command = build_helm_command(cfg)
         run(helm_command, cfg)
 
