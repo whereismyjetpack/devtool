b17ef3ea66165a947006b1017bb6511af394f0e7
Dann Bohn <dannbohn@gmail.com>
2019-02-23 19:37:30 -0500

    WIP init function

--- devtool/initialize.py
+++ devtool/initialize.py
@@ -1,2 +1,33 @@
-def initialize(cfg):
-    print("things")
+import os
+import click
+import subprocess
+
+def initialize(cfg, lang):
+    if lang == "java":
+        init_java(cfg)
+    elif lang == "golang":
+        print("we are initializing a golang project")
+
+
+def init_java(cfg):
+    if not os.path.isdir("config"):
+        click.echo(click.style("Creating config directory", fg="yellow"))
+        os.mkdir("config")
+
+    if not os.path.isfile("config/local.yml"):
+        click.echo(click.style("creating local configuratation", fg="yellow"))
+        values = subprocess.check_output(
+        f"helm inspect values {cfg['helm']['repo']['name']}/{cfg['helm']['chart']}".split())
+        with open("config/local.yml", "w") as f:
+            f.write(values.decode("utf-8"))
+
+    # if not os.path.isfile("Dockerfile"):
+    #     click.echo(click.style("Creating a Dockerfile", fg="yellow"))
+    #     with open("Dockerfile", "w") as f:
+
+
+ 
+
+        
+
+
