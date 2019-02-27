a34ca577ab96cad67b3cf001bcbe3265e8ab4aa3
Dann Bohn <dannbohn@gmail.com>
2019-02-26 21:26:46 -0500

    adds minikube_env

--- devtool/minikube.py
+++ devtool/minikube.py
@@ -4,9 +4,27 @@ import subprocess
 import click
 
 
+def minikube_env(cfg):
+    minikube = {}
+
+    env = "minikube docker-env".split()
+    try:
+        docker_env = subprocess.check_output(env).split()
+    except subprocess.CalledProcessError:
+        click.echo(click.style(f"minikube docker-env failed. using local docker daemon", fg="red"))
+        docker_env = []
+
+    for item in docker_env:
+        if "DOCKER" in item.decode("utf-8"):
+            k, v = item.decode("utf-8").split("=")
+            minikube[k] = str(v.strip('"'))
+
+    return minikube
+
+
 def minikube_setup(cfg):
     """
-    Starts minikube if not started, changes kubectl context to "minikube"
+    Starts minikube if not started
     """
     minikube = {}
     minikube_memory = cfg["minikube"]["memory"]
@@ -17,12 +35,6 @@ def minikube_setup(cfg):
         cmd = "minikube start --memory %s --cpus %s" % (minikube_memory, minikube_cpu)
         run(cmd.split(), cfg)
 
-    env = "minikube docker-env".split()
-    docker_env = subprocess.check_output(env).split()
-    for item in docker_env:
-        if "DOCKER" in item.decode("utf-8"):
-            k, v = item.decode("utf-8").split("=")
-            minikube[k] = str(v.strip('"'))
 
     current_context = subprocess.check_output(
         "kubectl config current-context".split()
