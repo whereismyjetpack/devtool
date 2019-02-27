4ea56eeeab9f4c1be8856cd82456766b64ff3c25
Dann Bohn <dannbohn@gmail.com>
2019-02-26 21:26:18 -0500

    adds more cli args, split out minikube env and setup

--- devtool/cli.py
+++ devtool/cli.py
@@ -3,11 +3,12 @@ from .initialize import initialize
 import yaml
 import os
 from .helm import build_helm_command, helm_setup
-from .minikube import minikube_setup
+from .minikube import minikube_setup, minikube_env
 from .command import run, check_output
 from .fluxhelmrelease import build_fhr
 import click
 from click_default_group import DefaultGroup
+from .emoji import rocket
 
 cfg = build_config()
 
@@ -62,7 +63,13 @@ def show_config():
 @click.option(
     "--skip-helm", default=False, is_flag=True, help="Skip helm install phase"
 )
-def build(skip_compile, skip_docker, skip_helm):
+@click.option(
+    "--skip-setup", default=False, is_flag=True, help="Skip minikube/kubectl setup"
+)
+@click.option(
+    "--suppress-output", default=False, is_flag=True, help="Suppress subprocess commands output"
+)
+def build(skip_compile, skip_docker, skip_helm, skip_setup, suppress_output):
 
     if cfg["skip"]["helm"]:
         skip_helm = True
@@ -70,9 +77,17 @@ def build(skip_compile, skip_docker, skip_helm):
         skip_docker = True
     if cfg["skip"]["compile"]:
         skip_compile = True
+    if cfg['skip']['setup']:
+        skip_setup = True
+    if suppress_output:
+        cfg['suppressoutput'] = True
 
-    minikube = minikube_setup(cfg)
-    helm_setup(cfg)
+    if skip_setup:
+        click.echo(click.style(f"Skipping Minikube/Helm setup\n", fg="red"))
+    else:
+        minikube = minikube_setup(cfg)
+        helm_setup(cfg)
+
 
     if skip_compile:
         click.echo(click.style("Skipping Compile Phase\n", fg="red"))
@@ -90,8 +105,26 @@ def build(skip_compile, skip_docker, skip_helm):
         click.echo(click.style("Skipping Docker Phase\n", fg="red"))
     else:
         click.echo(click.style("Building the Docker Container\n", fg="yellow"))
-        docker_command = f"docker --host={minikube['DOCKER_HOST']} --tlsverify={minikube['DOCKER_TLS_VERIFY']} --tlscacert={minikube['DOCKER_CERT_PATH']}/ca.pem --tlscert={minikube['DOCKER_CERT_PATH']}/cert.pem --tlskey={minikube['DOCKER_CERT_PATH']}/key.pem build -t {cfg['docker']['image']}:{cfg['docker']['tag']} ."
-        run(docker_command.split(), cfg)
+        minikube = minikube_env(cfg)
+        docker_command = []
+        docker_command.append("docker")
+        if minikube.get("DOCKER_HOST"):
+            docker_command.append(f"--host={minikube['DOCKER_HOST']}")
+        if minikube.get("DOCKER_TLS_VERIFY"):
+            docker_command.append(f"--tlsverify={minikube['DOCKER_TLS_VERIFY']}")
+        if minikube.get("DOCKER_CERT_PATH"):
+            docker_command.append(f"--tlscert={minikube['DOCKER_CERT_PATH']}/cert.pem")
+            docker_command.append(f"--tlscacert={minikube['DOCKER_CERT_PATH']}/ca.pem")
+            docker_command.append(f"--tlskey={minikube['DOCKER_CERT_PATH']}/key.pem")
+
+        docker_command.append("build")
+        docker_command.append("-t")
+        docker_command.append(f"{cfg['docker']['image']}:{cfg['docker']['tag']}")
+        docker_command.append(".")
+        
+        str_command = ' '.join(docker_command)
+        click.echo(click.style(f"Running the following docker command: {str_command}", fg="yellow"))
+        run(docker_command, cfg)
 
     if skip_helm:
         click.echo(click.style("Skipping Helm Phase\n", fg="red"))
@@ -106,7 +139,7 @@ def build(skip_compile, skip_docker, skip_helm):
         )
         run(helm_command, cfg)
 
-        click.echo("\U0001f680")
+        click.echo(rocket)
 
 
 if __name__ == "__main__":
